a8836c477d451efd81ef2d2aca50e8e46c154d29
hiroc <hirocfi@gmail.com>
2022-09-25 15:59:33 +0900

    2回目コミット

--- everyone_diary/settings.py
+++ everyone_diary/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!sx7#vy3zp&kjhmy!nz#719239a)_0b-8r-zkf17l6!!)(u7bi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
