7979c8062356276f6b0aac02546b511178758473
hiroc <hirocfi@gmail.com>
2022-09-26 09:41:08 +0900

    3回目コミット

--- diarys/urls.py
+++ diarys/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'diarys'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name = "index")
+    path('', views.IndexView.as_view(), name = "index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
 
     ]
